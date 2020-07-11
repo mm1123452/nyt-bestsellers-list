55685772a27f7cf637c236959ec3904458a4355e
Mabel <macmota@hotmail.com>
2020-07-11 14:29:10 -0400

    feat: implement 404 page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {Link} from 'react-router-dom'
+import illustration from '../images/illustration-404.svg'
+import './NotFound.css'
 
 function NotFound() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="notfound">
+      <img className="notfound__image" src={illustration}/>
+      <div className="notfound__content">
+        <h2 className="notfound__text">Sorry, we couldn't find that page</h2>
+        <Link className="notfound__link" to="/">Return to home page</Link>
+      </div>
     </div>
   );
 }
